29ac95cd6f6fe17f32caedb5b60f51dd01fd35d8
Ariel (Ori) Bar David <abardavi@redhat.com>
2021-03-30 22:49:49 +0300

    some stuff

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/abardavi/go-module
+
+go 1.14
+
+require (
+    github.com/abardavi/go-lib v1.2.3
+)
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "github.com/abardavi/go-lib/lib"
+    "fmt"
+)
+
+func main() {
+    fmt.Println("some lib info: ", lib.info)
+}
